484f816d43e935a81cd2fe5bddd674d42fc3889a
Ronald John Atanoso <ronaldjohnatanoso@gmail.com>
2025-09-29 18:53:22 +0800

    qdrant db init connection

---
+++ vector_db.py
@@ -0,0 +1,15 @@
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams, PointStruct
+
+class QdrantStorage:
+    def __init__(self, url="http://localhost:6333", collection="docs", dim=3072):
+        self.client = QdrantClient(url=url, timeout=30)
+        self.collection = collection
+        if not self.client.collection_exists(self.collection):
+            self.client.create_collection(
+                collection_name=self.collection,
+                vectors_config=VectorParams(
+                    size=dim,
+                    distance=Distance.COSINE
+                )
+            )
